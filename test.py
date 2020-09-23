4551e364d80c7626dea29de4e12b8783a3f4165c
Om Prakash Singh <55798799+ompsingh@users.noreply.github.com>
2020-09-22 20:02:28 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,6 @@
 from flask import *
-from flask_cors import CORS
-from pymongo import MongoClient
 import pandas as pd
 from flask import jsonify
-from bson.json_util import dumps
-import pulp
-from pulp import LpVariable,LpProblem,LpStatus,LpMaximize
-import pandas as pd
-import pymongo
-from pymongo import MongoClient
-from dateutil.relativedelta import relativedelta
-from datetime import date
-from datetime import datetime
 import json
 
 @app.route('/')
