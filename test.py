317e010b31bab922dda88f05042f48411e4b986c
Om Prakash Singh <55798799+ompsingh@users.noreply.github.com>
2020-09-22 20:16:11 -0400

    Update test.py

--- test.py
+++ test.py
@@ -2,11 +2,15 @@ from flask import *
 import pandas as pd
 from flask import jsonify
 import json
+from flask_cors import CORS
+app.secret_key='om2020@!'
+cors = CORS(app)
+authenticated = False
 
 @app.route('/')
 def home():
-    return "welcome to natwest backend"
+    return "welcome to Home"
 
 @app.route('/home')
 def home1():
-    return "welcome to natwest backend"
+    return "welcome to Home1"
